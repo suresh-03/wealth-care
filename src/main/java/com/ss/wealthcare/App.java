8f31e2b1d4ce0cb65e498f87747b42aca9e2b6e9
Suresh-03 <sureshkumar732003@gmail.com>
2024-12-07 17:00:41 +0530

    Added Project Structure

---
+++ src/main/java/com/ss/wealthcare/App.java
@@ -0,0 +1,16 @@
+package com.ss.wealthcare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a = 0;
+        System.out.println(a);
+        
+    }
+}
