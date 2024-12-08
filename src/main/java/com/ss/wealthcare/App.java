cccc078bd42be8f52cb35989a82b8576eb1fa3a7
mukesh-maharaja <mukeshcse94@gmail.com>
2024-12-08 15:22:03 +0530

    project submitted

--- src/main/java/com/ss/wealthcare/App.java
+++ src/main/java/com/ss/wealthcare/App.java
@@ -9,9 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        int a = 0;
+        int a = 54;
         System.out.println(a);
         int k = 10;
+        System.out.println("Hi guys");
         
     }
 }
